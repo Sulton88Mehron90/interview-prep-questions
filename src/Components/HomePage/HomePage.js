06d8140dc137e30aff81b342fc0372ee4473ce9f
Parvin Sattorova <nanajon66@gmail.com>
2023-11-08 06:39:17 -0700

    Add edits to home page files

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,11 +1,30 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import '../HomePage/HomePage.css';
+
+// export default function HomePage() {
+//   return (
+//     <div className="home">
+//       <h1 className="home__title">MOD4 Interview Prep</h1>
+//       <Link to="/flashcards" className="home__start-button">Let's Practice</Link>
+//     </div>
+//   );
+// }
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Chess from '../../Images/chess.png';
 import '../HomePage/HomePage.css';
 
 export default function HomePage() {
   return (
     <div className="home">
-      <h1 className="home__title">MOD4 Interview Prep</h1>
+      <div className="home__content">
+        <h1 className="home__title">
+          {/* MOD4 <img src={Chess} alt="Chess piece" className="home__image" /> Interview Prep */}
+          M <img src={Chess} alt="Chess piece" className="home__image" /> D4 Interview Prep
+        </h1>
+      </div>
       <Link to="/flashcards" className="home__start-button">Let's Practice</Link>
     </div>
   );
